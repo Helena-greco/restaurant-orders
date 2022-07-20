f11315c62d76110c5a37cc49cdc0880611d6fbff
Helena <nenafgreco@hotmail.com>
2022-07-20 13:35:38 -0400

    tratando erro de arquivo inexistente

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,13 @@
+import csv
+
+
 def analyze_log(path_to_file):
     if '.csv' not in path_to_file:
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+
+    try:
+        with open(path_to_file, encoding='utf-8') as csv_file:
+            file_reader = csv.reader(path_to_file, delimiter=",", quotechar='"')
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
