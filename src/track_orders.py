019efdfccbd993d15f489447222ba86b6bd64f98
Helena <nenafgreco@hotmail.com>
2022-07-20 15:45:50 -0400

    o prato mais pedido por cliente

--- src/track_orders.py
+++ src/track_orders.py
@@ -5,14 +5,26 @@ class TrackOrders:
     # aqui deve expor a quantidade de estoque
     def __len__(self):
         return len(self.data)
-        pass
 
     def add_new_order(self, customer, order, day):
-        self.data.append({"costumer": customer, "order": order, "day": day})
-        pass
+        self.data.append({"customer": customer, "order": order, "day": day})
 
+# Ref: https://pythonguides.com/python-find-max-value-in-a-dictionary/
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        most_ordered = dict()
+        order_list = list()
+
+        for order in self.data:
+            if order["customer"] == customer:
+                order_list.append(order)
+
+        for costumer in order_list:
+            if costumer["order"] not in most_ordered:
+                most_ordered[costumer["order"]] = 1
+            else:
+                most_ordered[costumer["order"]] += 1
+
+        return max(most_ordered, key=most_ordered.get)
 
     def get_never_ordered_per_customer(self, customer):
         pass
